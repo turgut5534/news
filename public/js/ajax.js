30738aefec761aa7577489b601f59f0ca6e2a8ee
turgut5534 <turgutsalgin5534@gmail.com>
2023-03-24 15:03:32 +0300

    news write form and back was fixed

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -30,3 +30,41 @@ $('.signin-form').on('submit', function(e) {
           }
     })
 })
+
+
+$('.news-form').on('submit', function(e) {
+
+    e.preventDefault()
+
+    $.ajax({
+        type: "POST", 
+        url : $(this).attr('action'),
+        data: $(this).serialize(),
+        beforeSend: function() {
+            $('#submitBtn').addClass('waiting').prop('disabled', true).html('Please wait...');
+        },
+        success: function(s) {
+
+            $('#submitBtn').removeClass('waiting').prop('disabled', false).html('Submit');
+
+            iziToast.success({
+                title: 'Başarılı',
+                message: s.message,
+            });
+
+            setTimeout(() => {
+              location.href = '/'
+            }, 1500)
+        },
+        error :function(xhr, status, error) {
+
+            $('#submitBtn').removeClass('waiting').prop('disabled', false).html('Submit');
+            const response = JSON.parse(xhr.responseText)
+  
+            iziToast.error({
+                  title: 'Hata',
+                  message: response.message,
+              });
+          }
+    })
+})
--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.get('/', isLoggedIn, (req,res) => {
 
     res.redirect('/news/all')
@@ -39,8 +38,45 @@ app.get('/news/write', (req,res) => {
     res.render('news_write')
 })
 
-app.get('/news/all', isLoggedIn ,async(req,res) => {
-    res.render('news')
+app.get('/news/all' ,async(req,res) => {
+
+    try {
+      const query = `SELECT * FROM news`;
+      const { rows:news } = await pool.query(query)
+
+      res.render('news', {
+        news: news
+      })
+    } catch(e) {
+      res.send('Hata')
+    }
+
+})
+
+app.get('/create', async(req,res) => {
+
+    try {
+      const query = `CREATE TABLE IF NOT EXISTS news (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        author INTEGER NOT NULL REFERENCES users(id),
+        image VARCHAR(255),
+        url VARCHAR(255),
+        lang VARCHAR(4),
+        published_date TIMESTAMP DEFAULT NOW(),
+        updated_data TIMESTAMP,
+        category VARCHAR(50) NOT NULL
+      );`
+
+      await pool.query(query)
+
+      return res.json({"status": "Table was created"})
+    }
+
+    catch(e) {
+      return res.json({"status": "Error"})
+    }
 })
 
 app.get('/login', (req,res) => {
@@ -72,9 +108,39 @@ app.post('/login', function(req, res, next) {
     })(req, res, next);
   });
 
-app.post('/save', (req,res) => {
-  console.log(req.body)
-})
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+  
+  app.post('/save', upload.single('image'), async(req, res) => {
+    
+    const { title, description, lang} = req.body
+
+    try {
+
+      const query = `INSERT INTO news (title, description, author, image, url, lang, published_date, updated_data, category)
+      VALUES ($1, $2, $3, $4, $5,$6,'2022-03-22 12:30:00', '2022-03-23 08:15:00', 'Health');
+      `;
+      
+      const values = [title, description, req.user.id, req.file.filename, req.file.filename, lang]
+  
+      await pool.query(query, values)
+  
+      res.send('ok')
+
+    } catch(e) {
+      res.send(e)
+    }
+
+  });
+  
 
 app.listen(port, ()=> {
     console.log(`Server is up on ${port}`)
